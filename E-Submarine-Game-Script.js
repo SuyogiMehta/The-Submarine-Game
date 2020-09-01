2917ed76158502c764eda582a1726580ed40516b
SuyogiMehta <68965718+SuyogiMehta@users.noreply.github.com>
2020-09-01 23:59:49 +0530

    Update E-Submarine-Game-Script.js

--- E-Submarine-Game-Script.js
+++ E-Submarine-Game-Script.js
@@ -33,14 +33,14 @@ function my2() {
     var differance = Number((youSubmarine3 - 70) - dS);
     if (differance < 0) {
         setTimeout(() => {
-            document.getElementById("youSubmarine").style.transition = "0.05s";
+            document.getElementById("youSubmarine").style.transition = "0.2s";
             document.getElementById("trial4").innerHTML = "2";
             my4()
         }, 0)
     }
     if (differance > 0) {
         setTimeout(() => {
-            document.getElementById("youSubmarine").style.transition = "0.05s";
+            document.getElementById("youSubmarine").style.transition = "0.2s";
             document.getElementById("trial4").innerHTML = "2";
             my5()
         }, 0)
